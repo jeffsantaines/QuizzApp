a625f90e05823d1f5f21f0dfc4e5d17cbd4dab4f
Jeff <jeffsantaines@gmail.com>
2017-11-27 06:56:32 +1300

    For submission trial 2, removing comments and unused classes

--- app/src/main/java/com/example/jeff/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/jeff/quizzapp/MainActivity.java
@@ -2,25 +2,15 @@ package com.example.jeff.quizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    // for future use only!
-  /*  ArrayList<QuestionAnswer> questionAnswers = new ArrayList<>();
-
-    QuestionAnswer questionAnswer1 = new QuestionAnswer("What type of roof is this?", "Hip");
-    QuestionAnswer questionAnswer2 = new QuestionAnswer("What is the minimum pitch for most high rib profile roof?", "3");
-    QuestionAnswer questionAnswer3 = new QuestionAnswer("Select the part of the roof that is highlighted in the drawing below", "eaves");
-    QuestionAnswer questionAnswer4 = new QuestionAnswer("The recommended minimum pitch for Corrugate longrun roofing is?", "8 degrees");
-    QuestionAnswer questionAnswer5 = new QuestionAnswer("Wind Zone is a factor to consider in roof construction.", "True");*/
 
     int testScore = 0;
 
@@ -28,17 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-//for future use only!
-/*      questionAnswers.add(questionAnswer1);
-        questionAnswers.add(questionAnswer2);
-        questionAnswers.add(questionAnswer3);
-        questionAnswers.add(questionAnswer4);
-        questionAnswers.add(questionAnswer5);
-
-        for (QuestionAnswer s : questionAnswers)
-            Log.v("Questionaire", s.getQuestion().toString() + " : " + s.getAnswer().toString() );*/
     }
 
     public void onSubmitButton1Clicked(View view) {
